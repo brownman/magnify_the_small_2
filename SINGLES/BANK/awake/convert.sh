dd91acd4c1c684d32625239e5ffb0fdb1ada0c82
brownman <brownman556@gmail.com>
2014-05-07 17:37:17 +0000

    [awake] automate sudoers file update for allowing non-root users permission to pm-utils operations

--- SINGLES/BANK/awake/convert.sh
+++ SINGLES/BANK/awake/convert.sh
@@ -1,10 +1,38 @@
 #!/bin/bash
+#depend: xsel 
 #info: 
-pushd `basename $0`>/dev/null
-file=$1
-user=$LOGNAME
-ls -l $file
-sudo chown $user $file
-sudo chgrp $user $file
-ls -l $file | grep root --color=auto
-popd>/dev/null
+set_env(){
+dir_self=`pwd`
+file_target=$dir_self/awake.sh
+file_sudoers=/etc/sudoers
+}
+
+
+permission(){
+    file=$file_target
+    user=root
+    #$LOGNAME
+    ls -l $file
+    ls -l $file | grep root --color=auto
+    sudo chown $user $file
+    sudo chgrp $user $file
+}
+
+sudoers(){
+    sudo cat $file_sudoers | grep $file_target --color=auto
+    echo "echo \"ALL    ALL = (root) NOPASSWD: $file_target\"" | xsel --clipboard
+    echo "[clipboard] updated !"
+    echo "[next] update file: $file_sudoers"
+}
+present(){
+type set_env
+type permission 
+type sudoers
+}
+steps(){
+    preset
+set_env
+permission
+sudoers
+}
+steps
